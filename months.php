27b7958a26cfb9f4dc61304ce2127ee7872445eb
jerryMuigai <35327734+jerryMuigai@users.noreply.github.com>
2018-02-28 14:46:57 +0300

    Update months.php

--- months.php
+++ months.php
@@ -6,40 +6,7 @@
 </head>
 <body>
 	<?php 
-		$years = date("Y");
-		$feb;
-		if ($years % 4 == 0) {
-			$feb = 29;
-		}
-		else{
-			$feb = 28;
-		}
 		
-		$months = [
-			"january" => 31,
-			"february" => $feb,
-			"march" => 31,
-			"april" => 30,
-			"may" => 31,
-			"june" => 30,
-			"july" => 31,
-			"august" => 31,
-			"september" => 30,
-			"october" => 31,
-			"november" => 30,
-			"december" => 31
-		];
-
-		function option($monthsArray){
-			foreach ($monthsArray as $month => $days) {
-				$Month = strtoupper($month);
-				echo "<option value='$month'>$Month</option>";
-			}
-		}
-
-	 ?>
-
-	 <div class="jumbotron container ">
 	 	<div class="text-center">
 	 	<div>
 	 	<table >
@@ -71,26 +38,7 @@
 
 
 	 <?php
-	 	if (isset($_POST['submit'])) {
-	 		$monthSelected = $_POST['month_dropdown'];
-	 		$days = $months[$monthSelected];
-
-	 		if ($monthSelected !== "february") {
-	 			echo "<div class='container'><h3>The month of $monthSelected has $days days</h3></div>";
-	 		}
-
-	 		else{
-	 			if ($days == 29) {
-	 				echo "<div class='container'><h3>The month of $monthSelected has $days days because it is a leap year</h3></div>";
-	 			}
-
-	 			else{
-	 				echo "<div class='container'><h3>The month of $monthSelected has $days days because it is not a leap year</h3></div>";
-	 			}
-	 		}
-	 		
-	 	}
-	 ?>
+	 	
 </div>
 
 
